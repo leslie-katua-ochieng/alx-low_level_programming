0d9ae01d09b35ee54197352e789a31f3c27a6092
leslie-katua-ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2022-10-16 21:56:06 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- *  *main - print the string into the output
- *    *Using the main function
- *     *this program prints out "Programming is like building a multilingual puzzle
- *      *Return: 0;
- *       */
+ *main - print the string into the output
+ *
+ *Using the main function
+ *this program prints out "Programming is like building a multilingual puzzle
+ *Return: 0;
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
