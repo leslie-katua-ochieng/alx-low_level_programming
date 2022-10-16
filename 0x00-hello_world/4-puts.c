1d3212aa069010c1c3548938b1d836c2ff80d91e
leslie-katua-ochieng <leslieochieng@kabarak.ac.ke>
2022-10-16 10:38:31 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  *main - print the string into the output
+ *   *
+ *    *Using the main function
+ *     *this program prints out "Programming is like building a multilingual puzzle
+ *      *Return: 0;
+ *       */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
