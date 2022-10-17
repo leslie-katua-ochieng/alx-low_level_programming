6d14493a1eb05d1d8fe3c9df415f46518dd72994
leslie-katua-ochieng <leslieochieng@kabarak.ac.ke>
2022-10-17 00:17:16 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+nclude <unistd.h>
+/**
+ *  *main - prints a quote "and that piece of art is beautiful"
+ *   *followed with a new line, to standard error
+ *    *
+ *     */
+int main(void)
+{
+	  write(2,
+			  	  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+				  	   59);
+	    
+	  return(1);
+}
