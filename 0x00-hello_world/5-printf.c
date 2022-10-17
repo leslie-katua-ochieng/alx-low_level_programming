ee55e444a85cb04d1311e18fa44454ff7750eb09
leslie-katua-ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2022-10-17 09:38:20 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
- *main - simple demonstration of printf
- *
- *Return: Always 0
- */
-
+*main - simple demonstration of printf
+*
+*Return: Always 0
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
