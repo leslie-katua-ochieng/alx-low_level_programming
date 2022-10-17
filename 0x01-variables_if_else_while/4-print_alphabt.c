4ad57ac91ec0cf1233fae123a51695c0d1639999
leslie-katua-ochieng <leslieochieng@kabarak.ac.ke>
2022-10-17 11:48:36 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  *main - Entry point
+ *   *
+ *    *Return: Always 0 (success)
+ *     *
+ *      */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'e' || letter == 'q')
+			continue;
+		else
+			putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
