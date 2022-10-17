88c3eb2b5f408b67091425f0d07f7fd0689a19ab
leslie-katua-ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2022-10-17 10:24:50 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 /**
- *  *main - prints a quote "and that piece of art is beautiful"
- *   *followed with a new line, to standard error
- *    *
- *     */
+*  *main - prints a quote "and that piece of art is beautiful"
+*   *followed with a new line, to standard error
+*    *
+*     */
 int main(void)
 {
-   write(2,
-	   "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	    59);
-	    
-	  return(1);
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
+
+return (1);
 }
