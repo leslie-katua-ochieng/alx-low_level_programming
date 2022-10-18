c5c90c6ee4f97193fc5606c89e0afb1b4e2b5438
leslie-katua-ochieng <leslieochieng@kabarak.ac.ke>
2022-10-18 01:22:05 -0700

    10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stidio.h>
+/* more headers goes there */
+/* betty style for function main goes there */
+/**
+ *  *main - Entry point
+ *   *
+ *    *Return: Always 0 (success)
+ *     *
+ *
+ */
+int main(void)
+{
+	int i = 48, j = 49;
+
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+		j = i + 1;
+	}
+	putchar('\n');
+	return (0);
+}
