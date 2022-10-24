e182c5984c371cbb3a97c86c7756feb447aa6c20
leslie-katua-ochieng <leslieochieng@kabarak.ac.ke>
2022-10-23 23:17:12 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line n long
+ *
+ * @n: length of line
+ *
+ * Return: always void
+ */
+void print_diagonal(int n)
+{
+	int i, spaceC;
+
+	i = 0;
+	while (n > 0 && i < n)
+	{
+		spaceC = 0;
+		while (spaceC < i)
+		{
+			_putchar(' ');
+			spaceC++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (n < 1)
+		_putchar('\n');
+}
