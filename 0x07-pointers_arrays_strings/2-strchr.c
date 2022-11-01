9f79d24a2e5b2566685c630ce1683e36dc3bd494
leslie ochieng <105386873+leslie-katua-ochieng@users.noreply.github.com>
2022-11-01 14:45:25 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include main.h"
 /**
 * *_strchr - search the first occurrence of the character in a string pointed
@@ -6,6 +5,7 @@
 *@c: character to search
 *Return: s (the first occuerrence of the char)
 */
+
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
